00765caa873afb19cf7e1fc36701e0d17a24653f
Im3rc <imerck13@gmail.com>
2020-11-02 22:24:13 +0530

    Camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-// import 'package:medicine_reminder/2NewPatient.dart';
+import 'package:medicine_reminder/2NewPatient.dart';
 // import 'package:medicine_reminder/timer/Timer.dart';
 // import 'package:medicine_reminder/enhancements/FirstPage.dart';
 // import 'package:medicine_reminder/navigation.dart';
@@ -21,7 +20,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Digital Medical Assistant',
       // home: FirstPage(),
-      home: SelectDay(),
+      home: NewPatient2(),
     );
   }
 }
