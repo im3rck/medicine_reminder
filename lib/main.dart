201e65e1e88ca83449be3108a552877752b5e03e
arcw146 <krithik1132@gmail.com>
2020-11-17 23:15:41 +0530

    Patient List modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:medicine_reminder/Authentication/Login.dart';
+import 'package:medicine_reminder/PatientList/FullPatientDetails.dart';
 import 'package:medicine_reminder/PatientList/NewPatient.dart';
 import 'package:medicine_reminder/PatientController/PatientAddPage.dart';
 import 'package:medicine_reminder/PatientList/PhasePage.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Digital Medical Assistant',
-      home: PhasePage(),
+      home: FullPatientDetails(),
       theme: ThemeData(fontFamily: 'Circular'),
     );
   }
