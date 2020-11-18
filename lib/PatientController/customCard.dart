1a87112a5d6142fd42cb2722c1554ca64de67608
Im3rc <imerck13@gmail.com>
2020-11-18 23:35:33 +0530

    UI

--- lib/PatientController/customCard.dart
+++ lib/PatientController/customCard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 Widget customCard(icon,String _title, String _subtitle){
   return Card(
--- lib/PatientList/DrawerScreen.dart
+++ lib/PatientList/DrawerScreen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:medicine_reminder/PatientController/PatientAddPage.dart';
+import 'package:medicine_reminder/PatientController/customCard.dart';
 import 'package:medicine_reminder/PatientList/configuration.dart';
 
 class DrawerScreen extends StatefulWidget {
@@ -10,7 +14,7 @@ class _DrawerScreenState extends State<DrawerScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: primaryGreen,
+      color: Color(0xff121212),
       padding: EdgeInsets.only(top: 50, bottom: 70, left: 10),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -30,7 +34,7 @@ class _DrawerScreenState extends State<DrawerScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Miroslava Savitskaya',
+                    'Care Giver Name',
                     style: TextStyle(
                         color: Colors.white, fontWeight: FontWeight.bold),
                   ),
@@ -41,7 +45,73 @@ class _DrawerScreenState extends State<DrawerScreen> {
               )
             ],
           ),
-          Column(
+          InkWell(
+            onTap: (){
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => PatientAddPage()));
+            },
+            child: Container(
+              alignment: Alignment.centerLeft,
+                height: ((MediaQuery.of(context).size.height) -
+                    50.0 -
+                    175.0) *
+                    .35,
+                width: (MediaQuery.of(context).size.width) *.85,
+                child: Card(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                  margin: EdgeInsets.all(5.0),
+                  color: Color(0xffbb86fc),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xff121212),
+                      border: Border.all(
+                          color: Color(0xffBB86FC),
+                          width: 1
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xff121212).withOpacity(0.2),
+                          spreadRadius: 3,
+                          blurRadius: 4,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Icon(FontAwesomeIcons.plus, color: Color(0xfff2e7fe), size: 30.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("Add",style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xfff2e7fe),
+                                height: 2,
+                              ),),
+                              SizedBox(height: 1.0,),
+                              Text("New Patient", style: TextStyle(
+                                fontSize: 14,
+                                color: Color(0xfff2e7fe).withOpacity(0.6),
+                                height: 2,
+                              ),)
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+
+
+
+                ),),
+          ),
+         /* Column(
             children: drawerItems
                 .map((element) => Padding(
                       padding: const EdgeInsets.all(18.0),
@@ -64,7 +134,7 @@ class _DrawerScreenState extends State<DrawerScreen> {
                       ),
                     ))
                 .toList(),
-          ),
+          ),*/
           Row(
             children: [
               Icon(
