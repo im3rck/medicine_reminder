7e7509923d2e4b3886a08159381aa1bb2946e1fa
arcw146 <krithik1132@gmail.com>
2020-11-18 18:09:48 +0530

    Patient Controller Modifications

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Digital Medical Assistant',
-       home: PhasePage(),
+       home: PatientAddPage(),
     );
   }
 }
