b3cdc4d2ac550c68115d0d8f0c25cec1f85512b1
KarthikRamanan16 <karthikramanann@gmail.com>
2020-11-20 16:22:30 +0530

    Launch Screen Added

---
+++ lib/LaunchScreen/Launch.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+//import 'package:medicine_reminder/LaunchScreen/_shared/lib/env.dart';
+import 'package:medicine_reminder/LaunchScreen/env.dart';
+import 'demo.dart';
+
+
+class App extends StatelessWidget {
+
+  static String _pkg = "gooey_edge";
+  static String get pkg => Env.getPackage(_pkg);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: GooeyEdgeDemo(title: 'Gooey Edge Demo'),
+    );
+  }
+}
+
