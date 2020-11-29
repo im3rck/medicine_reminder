697ef4c7a1d984f027be2f114a06bd5fb4b10758
Im3rc <imerck13@gmail.com>
2020-11-29 10:13:05 +0530

    Drawer Screen

--- lib/PatientList/DrawerScreen.dart
+++ lib/PatientList/DrawerScreen.dart
@@ -83,7 +83,7 @@ class _DrawerScreenState extends State<DrawerScreen> {
           // ),
           Flexible(
             child: FractionallySizedBox(
-              heightFactor: 0.6,
+              heightFactor: 0.7,
               child: Column(
                 children: drawerItems
                     .map((element) => Padding(
